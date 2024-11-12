09f223309bbf8718ec0be98178439a6703244eb1
Elisa Cuevas <elisa.cuefe@ciencias.unam.mx>
2024-11-12 11:41:26 -0600

    Agregue ejercicio 5

--- Arboles.hs
+++ Arboles.hs
@@ -11,8 +11,8 @@ longitud (Raiz _ izquierdo derecho) = 1 + longitud izquierdo + longitud derecho
 profundidad :: Arbol a -> Int
 profundidad ArbolVacio = 0
 profundidad (Raiz _ izquierdo derecho) =  if longitud derecho > longitud izquierdo 
-    then longitud derecho + 1
-    else longitud izquierdo + 1
+                                            then longitud derecho + 1
+                                            else longitud izquierdo + 1
 
 
 {-Ejercicio 3-}
@@ -28,6 +28,25 @@ data TipoRecorrido = InOrder | PreOrder | PostOrder deriving Show
 
 recorrido :: Arbol a -> TipoRecorrido -> [a]
 recorrido ArbolVacio _ = []
-recorrido (Raiz a izquierdo derecho) InOrder = recorrido izquierdo InOrder ++ [a] ++ recorrido derecho InOrder
-recorrido (Raiz a izquierdo derecho) PreOrder = [a] ++ recorrido izquierdo PreOrder ++ recorrido derecho PreOrder
-recorrido (Raiz a izquierdo derecho) PostOrder = recorrido izquierdo PostOrder ++ recorrido derecho PostOrder ++ [a]
+recorrido (Raiz a izquierdo derecho) InOrder = recorrido izquierdo InOrder 
+                                                ++ [a] 
+                                                ++ recorrido derecho InOrder
+recorrido (Raiz a izquierdo derecho) PreOrder = [a] 
+                                                ++ recorrido izquierdo PreOrder 
+                                                ++ recorrido derecho PreOrder
+recorrido (Raiz a izquierdo derecho) PostOrder = recorrido izquierdo PostOrder 
+                                                ++ recorrido derecho PostOrder 
+                                                ++ [a]
+
+{-Ejercicio 5-}
+niveles :: Arbol a -> [[a]]
+niveles ArbolVacio = []
+niveles (Raiz a ArbolVacio ArbolVacio) = [[a]]
+niveles (Raiz a izquierdo derecho) = [a] 
+                                        : combinarNiveles (niveles izquierdo) 
+                                                         (niveles derecho)
+
+combinarNiveles :: [[a]] -> [[a]] -> [[a]]
+combinarNiveles [] ys = ys
+combinarNiveles xs [] = xs
+combinarNiveles (x:xs) (y:ys) = (x ++ y) : combinarNiveles xs ys
